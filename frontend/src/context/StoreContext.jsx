0461cbb47569dfdc4223e3c2dae3dc9929e016bd
Pydeti Yeswanth Sai RaghuRam <119839691+yeswanthsairaghuram@users.noreply.github.com>
2025-07-02 22:11:13 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-    const url = "https://food-del-backend-2l3u.onrender.com";
+    const url = "https://food-del-backend-2l3u.onrender.com/";
 
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("");
