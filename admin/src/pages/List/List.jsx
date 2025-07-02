ee68671d91bf0ff3d796b8c3c1d144f839fee60f
Pydeti Yeswanth Sai RaghuRam <119839691+yeswanthsairaghuram@users.noreply.github.com>
2025-07-02 22:29:17 +0530

    Update List.jsx

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import './list.css'
 import axios from 'axios'
 import { toast } from 'react-toastify'
-import {assets} from '../../assets/assets'
 const List = ({url}) => {
     
     const [list, setList] = useState([]);
