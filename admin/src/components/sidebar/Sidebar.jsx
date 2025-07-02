41d20101447bc4b937b8d2d1f885195715a39d9d
Pydeti Yeswanth Sai RaghuRam <119839691+yeswanthsairaghuram@users.noreply.github.com>
2025-07-02 22:21:30 +0530

    Update Sidebar.jsx

--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Sidebar.css'
+import './sidebar.css'
 import { assets } from '../../assets/assets'
 import { NavLink } from 'react-router-dom'
 const Sidebar = () => {
